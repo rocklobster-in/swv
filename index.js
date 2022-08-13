c7304a033d27ab46add59c35b63e42e6d1c84def
Takayuki Miyoshi <takayukister@gmail.com>
2022-08-13 18:32:43 +0900

    Add files

---
+++ index.js
@@ -0,0 +1,57 @@
+import FormDataTree from '@takayukister/form-data-tree';
+
+import * as validators from './rules';
+import { ValidationError } from './error';
+
+const validate = ( schema, formData, options = {} ) => {
+	const { targetFields } = options;
+
+	const rules = ( schema.rules ?? [] ).filter(
+		( { rule, ...properties } ) => {
+			if ( 'function' !== typeof validators[ rule ] ) {
+				return false;
+			}
+
+			if ( 'function' === typeof validators[ rule ].matches ) {
+				return validators[ rule ].matches( properties, options );
+			}
+
+			if ( Array.isArray( targetFields ) && targetFields.length ) {
+				return targetFields.includes( properties.field );
+			}
+
+			return true;
+		}
+	);
+
+	if ( ! rules.length ) { // There is no rule to validate.
+		return new Map();
+	}
+
+	const formDataTree = new FormDataTree( formData );
+
+	const result = rules.reduce( ( prev, current ) => {
+		const { rule, ...properties } = current;
+
+		if ( prev.get( properties.field )?.error ) {
+			return prev;
+		}
+
+		try {
+			validators[ rule ].call( { rule, ...properties }, formDataTree );
+		} catch ( error ) {
+			if ( error instanceof ValidationError ) {
+				return prev.set( properties.field, error );
+			}
+		}
+
+		return prev.set( properties.field, {} );
+	}, new Map() );
+
+	return result;
+};
+
+export { 
+	validators,
+	validate,
+};
