fcda4fecee9c07a0e5305903045abb209890b818
Takayuki Miyoshi <takayukister@gmail.com>
2024-07-25 18:33:45 +0900

    Bulk trim text values
    Closes #31

--- rules/dayofweek.js
+++ rules/dayofweek.js
@@ -1,12 +1,13 @@
 import { ValidationError } from '../error';
 
 export const dayofweek = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field );
+	const values = formDataTree.getAll( this.field )
+		.map( val => val.trim() ).filter( val => '' !== val );
 
 	const convertToIso8601 = jsDow => ( 0 === jsDow ) ? 7 : jsDow;
 
 	const isAcceptableValue = value => {
-		const date = new Date( value.trim() );
+		const date = new Date( value );
 		const day = convertToIso8601( date.getDay() );
 
 		return this.accept?.some(
--- rules/maxitems.js
+++ rules/maxitems.js
@@ -1,9 +1,8 @@
 import { ValidationError } from '../error';
 
 export const maxitems = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field ).filter(
-		val => '' !== val.trim()
-	);
+	const values = formDataTree.getAll( this.field )
+		.map( val => val.trim() ).filter( val => '' !== val );
 
 	if ( parseInt( this.threshold ) < values.length ) {
 		throw new ValidationError( this );
