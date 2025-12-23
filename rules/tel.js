1442478a8dd4baf4d7700928f0bc7d7b65c266bb
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-23 18:19:32 +0900

    Update tel.js

--- rules/tel.js
+++ rules/tel.js
@@ -1,31 +1,61 @@
+import FormDataTree from '@rocklobsterinc/form-data-tree';
+
+import { AbstractRule } from '../abstract-rule';
 import { InvalidityException as Invalidity } from '../invalidity-exception';
+import { flattenTree } from '../helpers';
 
-export const tel = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field )
-		.map( val => val.trim() ).filter( val => '' !== val );
+export function TelRule( properties ) {
+	this.field = properties.field;
+	this.error = properties.error;
+}
 
-	const isTelephoneNumber = text => {
-		text = text.replace( /[#*].*$/, '' ); // Remove extension
-		text = text.replaceAll( /[()/.*#\s-]+/g, '' );
+Object.setPrototypeOf( TelRule.prototype, AbstractRule.prototype );
 
-		const international = text.startsWith( '+' ) || text.startsWith( '00' );
 
-		if ( international ) {
-			text = `+${ text.replace( /^[+0]+/, '' ) }`;
-		}
+/**
+ * Returns true if the given string is a well-formed telephone number.
+ *
+ * @param {string} text - String to check.
+ */
+TelRule.isTel = text => {
+	text = text.replace( /[#*].*$/, '' ); // Remove extension
+	text = text.replaceAll( /[()/.*#\s-]+/g, '' );
 
-		if ( ! /^[+]?[0-9]+$/.test( text ) ) {
-			return false;
-		}
+	const international = text.startsWith( '+' ) || text.startsWith( '00' );
 
-		if ( ! ( 5 < text.length && text.length < 16 ) ) {
-			return false;
-		}
-
-		return true;
-	};
-
-	if ( ! values.every( isTelephoneNumber ) ) {
-		throw new Invalidity( this );
+	if ( international ) {
+		text = `+${ text.replace( /^[+0]+/, '' ) }`;
 	}
+
+	if ( ! /^[+]?[0-9]+$/.test( text ) ) {
+		return false;
+	}
+
+	if ( ! ( 5 < text.length && text.length < 16 ) ) {
+		return false;
+	}
+
+	return true;
 };
+
+
+/**
+ * Validates the form data according to the logic defined by the rule.
+ *
+ * @param {Object} formDataTree - FormDataTree object to validate.
+ */
+TelRule.prototype.validate = function ( formDataTree, context ) {
+	const values = flattenTree( formDataTree.getAll( this.field ) );
+
+	if ( ! values.length ) {
+		return true;
+	}
+
+	for ( const value of values ) {
+		if ( ! TelRule.isTel( value ) ) {
+			throw new Invalidity( { ...this, cause: value } );
+		}
+	}
+
+	return true;
+}
