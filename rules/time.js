f7f58d27f5754267bb462f406ccc377dfd4ed484
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-23 18:26:26 +0900

    Update time.js

--- rules/time.js
+++ rules/time.js
@@ -1,28 +1,59 @@
+import FormDataTree from '@rocklobsterinc/form-data-tree';
+
+import { AbstractRule } from '../abstract-rule';
 import { InvalidityException as Invalidity } from '../invalidity-exception';
+import { flattenTree } from '../helpers';
 
-export const time = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field )
-		.map( val => val.trim() ).filter( val => '' !== val );
+export function TimeRule( properties ) {
+	this.field = properties.field;
+	this.error = properties.error;
+}
 
-	// https://html.spec.whatwg.org/multipage/input.html#time-state-(type=time)
-	const isValidTimeString = text => {
-		const pattern = /^([0-9]{2})\:([0-9]{2})(?:\:([0-9]{2}))?$/;
-		const matches = text.match( pattern );
+Object.setPrototypeOf( TimeRule.prototype, AbstractRule.prototype );
 
-		if ( ! matches ) {
-			return false;
-		}
 
-		const hour = parseInt( matches[1] );
-		const minute = parseInt( matches[2] );
-		const second = matches[3] ? parseInt( matches[3] ) : 0;
+/**
+ * Returns true if the given string is a valid time.
+ *
+ * @link https://html.spec.whatwg.org/multipage/input.html#time-state-(type=time)
+ *
+ * @param {string} text - String to check.
+ */
+TimeRule.isTime = text => {
+	const pattern = /^([0-9]{2})\:([0-9]{2})(?:\:([0-9]{2}))?$/;
+	const matches = text.match( pattern );
 
-		return 0 <= hour && hour <= 23 &&
-			0 <= minute && minute <= 59 &&
-			0 <= second && second <= 59;
-	};
-
-	if ( ! values.every( isValidTimeString ) ) {
-		throw new Invalidity( this );
+	if ( ! matches ) {
+		return false;
 	}
+
+	const hour = parseInt( matches[1] );
+	const minute = parseInt( matches[2] );
+	const second = matches[3] ? parseInt( matches[3] ) : 0;
+
+	return 0 <= hour && hour <= 23 &&
+		0 <= minute && minute <= 59 &&
+		0 <= second && second <= 59;
 };
+
+
+/**
+ * Validates the form data according to the logic defined by the rule.
+ *
+ * @param {Object} formDataTree - FormDataTree object to validate.
+ */
+DateRule.prototype.validate = function ( formDataTree, context ) {
+	const values = flattenTree( formDataTree.getAll( this.field ) );
+
+	if ( ! values.length ) {
+		return true;
+	}
+
+	for ( const value of values ) {
+		if ( ! TimeRule.isTime( value ) ) {
+			throw new Invalidity( { ...this, cause: value } );
+		}
+	}
+
+	return true;
+}
