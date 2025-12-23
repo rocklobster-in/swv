6a9fd0bfb08d522874f45102636922881561c510
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-23 18:38:15 +0900

    Update url.js

--- rules/url.js
+++ rules/url.js
@@ -1,27 +1,52 @@
+import FormDataTree from '@rocklobsterinc/form-data-tree';
+
+import { AbstractRule } from '../abstract-rule';
 import { InvalidityException as Invalidity } from '../invalidity-exception';
+import { flattenTree } from '../helpers';
 
-export const url = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field )
-		.map( val => val.trim() ).filter( val => '' !== val );
+export function URLRule( properties ) {
+	this.field = properties.field;
+	this.error = properties.error;
+}
 
-	const isAbsoluteUrl = text => {
-		try {
-			const urlObj = new URL( text );
-			const protocol = urlObj.protocol.replace( /:$/, '' );
-			return isAllowedProtocol( protocol );
-		} catch {
-			return false;
-		}
-	};
+Object.setPrototypeOf( URLRule.prototype, AbstractRule.prototype );
 
-	const isAllowedProtocol = protocol => {
-		// https://developer.wordpress.org/reference/functions/wp_allowed_protocols/
-		const allowedProtocols = [ 'http', 'https', 'ftp', 'ftps', 'mailto', 'news', 'irc', 'irc6', 'ircs', 'gopher', 'nntp', 'feed', 'telnet', 'mms', 'rtsp', 'sms', 'svn', 'tel', 'fax', 'xmpp', 'webcal', 'urn' ];
 
-		return -1 !== allowedProtocols.indexOf( protocol );
-	};
+/**
+ * Returns true if the given string is a well-formed URL.
+ *
+ * @param {string} text - String to check.
+ */
+URLRule.isUrl = text => {
+	const allowedProtocols = [ 'http', 'https' ];
 
-	if ( ! values.every( isAbsoluteUrl ) ) {
-		throw new Invalidity( this );
+	try {
+		const urlObj = new URL( text );
+		const protocol = urlObj.protocol.replace( /:$/, '' );
+		return allowedProtocols.includes( protocol );
+	} catch {
+		return false;
 	}
 };
+
+
+/**
+ * Validates the form data according to the logic defined by the rule.
+ *
+ * @param {Object} formDataTree - FormDataTree object to validate.
+ */
+URLRule.prototype.validate = function ( formDataTree, context ) {
+	const values = flattenTree( formDataTree.getAll( this.field ) );
+
+	if ( ! values.length ) {
+		return true;
+	}
+
+	for ( const value of values ) {
+		if ( ! URLRule.isUrl( value ) ) {
+			throw new Invalidity( { ...this, cause: value } );
+		}
+	}
+
+	return true;
+}
