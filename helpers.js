4c6a76beb3e8ebf803a00dce85dde3920b7da6d8
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-23 17:36:23 +0900

    Update date.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,22 @@
+
+/**
+ * Converts a multi-layered map to a flat array.
+ */
+export const flattenTree = ( tree ) => {
+	if ( tree instanceof Map ) {
+		const result = [];
+
+		for ( const [ key, value ] of tree ) {
+			result.push( ...flattenTree( value ) );
+		}
+
+		return result;
+	}
+
+	return [ tree ];
+};
+
+
 export const getMimeTypes = () => {
 	const mimeTypes = new Map();
 
--- rules/date.js
+++ rules/date.js
@@ -1,21 +1,51 @@
+import FormDataTree from '@rocklobsterinc/form-data-tree';
+
+import { AbstractRule } from '../abstract-rule';
 import { InvalidityException as Invalidity } from '../invalidity-exception';
+import { flattenTree } from '../helpers';
 
-export const date = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field )
-		.map( val => val.trim() ).filter( val => '' !== val );
+export function DateRule( properties ) {
+	this.field = properties.field;
+	this.error = properties.error;
+}
 
-	// https://html.spec.whatwg.org/multipage/input.html#date-state-(type=date)
-	const isValidDateString = text => {
-		if ( ! /^[0-9]{4,}-[0-9]{2}-[0-9]{2}$/.test( text ) ) {
-			return false;
-		}
+Object.setPrototypeOf( DateRule.prototype, AbstractRule.prototype );
 
-		const date = new Date( text );
 
-		return ! Number.isNaN( date.valueOf() );
-	};
-
-	if ( ! values.every( isValidDateString ) ) {
-		throw new Invalidity( this );
+/**
+ * Returns true if the given string is a valid Gregorian date in
+ * the YYYY-MM-DD format.
+ *
+ * @param {string} text - String to check.
+ */
+DateRule.isDate = text => {
+	if ( ! /^[0-9]{4,}-[0-9]{2}-[0-9]{2}$/.test( text ) ) {
+		return false;
 	}
+
+	const date = new Date( text );
+
+	return ! Number.isNaN( date.valueOf() );
 };
+
+
+/**
+ * Validates the form data according to the logic defined by the rule.
+ *
+ * @param {Object} formDataTree - FormDataTree object to validate.
+ */
+DateRule.prototype.validate = function ( formDataTree, context ) {
+	const values = flattenTree( formDataTree.getAll( this.field ) );
+
+	if ( ! values.length ) {
+		return true;
+	}
+
+	for ( const value of values ) {
+		if ( ! DateRule.isDate( value ) ) {
+			throw new Invalidity( { ...this, cause: value } );
+		}
+	}
+
+	return true;
+}
