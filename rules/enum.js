31e1776405159f510ff1e30cf4f1108ed0bfee23
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-24 18:02:02 +0900

    Update enum.js

--- rules/enum.js
+++ rules/enum.js
@@ -1,14 +1,35 @@
+import FormDataTree from '@rocklobsterinc/form-data-tree';
+
+import { AbstractRule } from '../abstract-rule';
 import { InvalidityException as Invalidity } from '../invalidity-exception';
+import { flattenTree } from '../helpers';
 
-export const enumeration = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field )
-		.map( val => val.trim() ).filter( val => '' !== val );
+export function EnumRule( properties ) {
+	this.field = properties.field;
+	this.error = properties.error;
+	this.accept = properties.accept;
+}
 
-	const isAcceptableValue = value => this.accept?.some(
-		acceptableValue => value === String( acceptableValue )
-	);
+Object.setPrototypeOf( EnumRule.prototype, AbstractRule.prototype );
 
-	if ( ! values.every( isAcceptableValue ) ) {
-		throw new Invalidity( this );
+
+/**
+ * Validates the form data according to the logic defined by the rule.
+ *
+ * @param {Object} formDataTree - FormDataTree object to validate.
+ */
+EnumRule.prototype.validate = function ( formDataTree, context ) {
+	const values = flattenTree( formDataTree.getAll( this.field ) );
+
+	if ( ! values.length ) {
+		return true;
 	}
-};
+
+	for ( const value of values ) {
+		if ( ! this.accept?.map( String ).includes( value ) ) {
+			throw new Invalidity( { ...this, cause: value } );
+		}
+	}
+
+	return true;
+}
