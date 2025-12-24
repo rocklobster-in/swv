842a515dad8dbd9ea5bde0b711f4b20583fd8400
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-24 17:38:41 +0900

    Update requiredfile.js

--- rules/requiredfile.js
+++ rules/requiredfile.js
@@ -1,9 +1,28 @@
+import FormDataTree from '@rocklobsterinc/form-data-tree';
+
+import { AbstractRule } from '../abstract-rule';
 import { InvalidityException as Invalidity } from '../invalidity-exception';
+import { flattenTree } from '../helpers';
 
-export const requiredfile = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field );
+export function RequiredFileRule( properties ) {
+	this.field = properties.field;
+	this.error = properties.error;
+}
 
-	if ( 0 === values.length ) {
-		throw new Invalidity( this );
+Object.setPrototypeOf( RequiredFileRule.prototype, AbstractRule.prototype );
+
+
+/**
+ * Validates the form data according to the logic defined by the rule.
+ *
+ * @param {Object} formDataTree - FormDataTree object to validate.
+ */
+RequiredFileRule.prototype.validate = function ( formDataTree, context ) {
+	const files = flattenTree( formDataTree.getAllFiles( this.field ) );
+
+	if ( ! files.length ) {
+		throw new Invalidity( { ...this } );
 	}
-};
+
+	return true;
+}
