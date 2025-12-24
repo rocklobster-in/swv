8369ed7c4dc7b9d7dcb01e06a2330f6278f592ef
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-24 17:45:29 +0900

    Update number.js

--- rules/number.js
+++ rules/number.js
@@ -1,23 +1,54 @@
+import FormDataTree from '@rocklobsterinc/form-data-tree';
+
+import { AbstractRule } from '../abstract-rule';
 import { InvalidityException as Invalidity } from '../invalidity-exception';
+import { flattenTree } from '../helpers';
 
-export const number = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field )
-		.map( val => val.trim() ).filter( val => '' !== val );
+export function NumberRule( properties ) {
+	this.field = properties.field;
+	this.error = properties.error;
+}
 
-	// https://html.spec.whatwg.org/multipage/input.html#number-state-(type=number)
-	const isValidFloatingPointNumber = text => {
-		if ( /^[-]?[0-9]+(?:[eE][+-]?[0-9]+)?$/.test( text ) ) {
-			return true;
-		}
+Object.setPrototypeOf( NumberRule.prototype, AbstractRule.prototype );
 
-		if ( /^[-]?(?:[0-9]+)?[.][0-9]+(?:[eE][+-]?[0-9]+)?$/.test( text ) ) {
-			return true;
-		}
 
-		return false;
-	};
-
-	if ( ! values.every( isValidFloatingPointNumber ) ) {
-		throw new Invalidity( this );
+/**
+ * Returns true if the given string is a well-formed number.
+ *
+ * @link https://html.spec.whatwg.org/multipage/input.html#number-state-(type=number)
+ *
+ * @param {string} text - String to check.
+ */
+NumberRule.isNumber = text => {
+	if ( /^[-]?[0-9]+(?:[eE][+-]?[0-9]+)?$/.test( text ) ) {
+		return true;
 	}
+
+	if ( /^[-]?(?:[0-9]+)?[.][0-9]+(?:[eE][+-]?[0-9]+)?$/.test( text ) ) {
+		return true;
+	}
+
+	return false;
 };
+
+
+/**
+ * Validates the form data according to the logic defined by the rule.
+ *
+ * @param {Object} formDataTree - FormDataTree object to validate.
+ */
+NumberRule.prototype.validate = function ( formDataTree, context ) {
+	const values = flattenTree( formDataTree.getAll( this.field ) );
+
+	if ( ! values.length ) {
+		return true;
+	}
+
+	for ( const value of values ) {
+		if ( ! NumberRule.isNumber( value ) ) {
+			throw new Invalidity( { ...this, cause: value } );
+		}
+	}
+
+	return true;
+}
