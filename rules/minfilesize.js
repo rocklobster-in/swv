9ef0024b890bd2f1108c89408fc0c286b07136ab
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-25 17:32:14 +0900

    Update minfilesize.js

--- rules/minfilesize.js
+++ rules/minfilesize.js
@@ -1,17 +1,41 @@
+import FormDataTree from '@rocklobsterinc/form-data-tree';
+
+import { AbstractRule } from '../abstract-rule';
 import { InvalidityException as Invalidity } from '../invalidity-exception';
+import { flattenTree } from '../helpers';
 
-export const minfilesize = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field );
+export function MinFilesizeRule( properties ) {
+	this.field = properties.field;
+	this.error = properties.error;
+	this.threshold = properties.threshold;
+}
 
-	let totalVolume = 0;
+Object.setPrototypeOf( MinFilesizeRule.prototype, AbstractRule.prototype );
 
-	values.forEach( file => {
-		if ( file instanceof File ) {
-			totalVolume += file.size;
+
+/**
+ * Validates the form data according to the logic defined by the rule.
+ *
+ * @param {Object} formDataTree - FormDataTree object to validate.
+ */
+MinFilesizeRule.prototype.validate = function ( formDataTree, context ) {
+	const files = flattenTree( formDataTree.getAllFiles( this.field ) );
+
+	if ( ! files.length ) {
+		return true;
+	}
+
+	const totalVolume = files.reduce( ( accumulator, current ) => {
+		if ( current instanceof File ) {
+			accumulator += current.size;
 		}
-	} );
+
+		return accumulator;
+	}, 0 );
 
 	if ( totalVolume < parseInt( this.threshold ) ) {
-		throw new Invalidity( this );
+		throw new Invalidity( { ...this } );
 	}
-};
+
+	return true;
+}
