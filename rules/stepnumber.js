ba01c361a8ba8a88ba445d5d4ed28fe63e78f53b
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-25 13:41:59 +0900

    Update stepnumber.js

--- rules/stepnumber.js
+++ rules/stepnumber.js
@@ -1,8 +1,30 @@
-import { InvalidityException as Invalidity } from '../invalidity-exception';
+import FormDataTree from '@rocklobsterinc/form-data-tree';
 
-export const stepnumber = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field )
-		.map( val => val.trim() ).filter( val => '' !== val );
+import { AbstractRule } from '../abstract-rule';
+import { InvalidityException as Invalidity } from '../invalidity-exception';
+import { flattenTree } from '../helpers';
+
+export function StepNumberRule( properties ) {
+	this.field = properties.field;
+	this.error = properties.error;
+	this.base = properties.base;
+	this.interval = properties.interval;
+}
+
+Object.setPrototypeOf( StepNumberRule.prototype, AbstractRule.prototype );
+
+
+/**
+ * Validates the form data according to the logic defined by the rule.
+ *
+ * @param {Object} formDataTree - FormDataTree object to validate.
+ */
+StepNumberRule.prototype.validate = function ( formDataTree, context ) {
+	const values = flattenTree( formDataTree.getAll( this.field ) );
+
+	if ( ! values.length ) {
+		return true;
+	}
 
 	const base = parseFloat( this.base );
 	const interval = parseFloat( this.interval );
@@ -24,7 +46,11 @@ export const stepnumber = function ( formDataTree ) {
 		return false;
 	};
 
-	if ( ! values.every( matchesStep ) ) {
-		throw new Invalidity( this );
+	for ( const value of values ) {
+		if ( ! matchesStep( value ) ) {
+			throw new Invalidity( { ...this, cause: value } );
+		}
 	}
-};
+
+	return true;
+}
