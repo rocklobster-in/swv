004c5bed44bdcd9b9982ef50f50054d0dc802b1c
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-25 13:34:14 +0900

    Update dayofweek.js

--- rules/dayofweek.js
+++ rules/dayofweek.js
@@ -1,21 +1,40 @@
-import { InvalidityException as Invalidity } from '../invalidity-exception';
+import FormDataTree from '@rocklobsterinc/form-data-tree';
 
-export const dayofweek = function ( formDataTree ) {
-	const values = formDataTree.getAll( this.field )
-		.map( val => val.trim() ).filter( val => '' !== val );
+import { AbstractRule } from '../abstract-rule';
+import { InvalidityException as Invalidity } from '../invalidity-exception';
+import { flattenTree } from '../helpers';
+
+export function DayofweekRule( properties ) {
+	this.field = properties.field;
+	this.error = properties.error;
+	this.accept = properties.accept;
+}
+
+Object.setPrototypeOf( DayofweekRule.prototype, AbstractRule.prototype );
+
+
+/**
+ * Validates the form data according to the logic defined by the rule.
+ *
+ * @param {Object} formDataTree - FormDataTree object to validate.
+ */
+DayofweekRule.prototype.validate = function ( formDataTree, context ) {
+	const values = flattenTree( formDataTree.getAll( this.field ) );
+
+	if ( ! values.length ) {
+		return true;
+	}
 
 	const convertToIso8601 = jsDow => ( 0 === jsDow ) ? 7 : jsDow;
 
-	const isAcceptableValue = value => {
+	for ( const value of values ) {
 		const date = new Date( value );
 		const day = convertToIso8601( date.getDay() );
 
-		return this.accept?.some(
-			acceptableValue => day === parseInt( acceptableValue )
-		);
-	};
-
-	if ( ! values.every( isAcceptableValue ) ) {
-		throw new Invalidity( this );
+		if ( ! this.accept?.map( String ).includes( String( day ) ) ) {
+			throw new Invalidity( { ...this, cause: value } );
+		}
 	}
-};
+
+	return true;
+}
