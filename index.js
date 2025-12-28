2bfa5116f93ab2a125aef298da79fd002a9c1592
Takayuki Miyoshi <takayukister@gmail.com>
2025-12-28 12:03:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export { InvalidityException } from './invalidity-exception';
 export { AbstractRule } from './abstract-rule';
 export { CompositeRule } from './composite-rule';
-export * from './rules';
+export * as rules from './rules';
