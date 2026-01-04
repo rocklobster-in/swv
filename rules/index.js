ae819cc564a1b51d601222c0ff356dc51d943b6d
Takayuki Miyoshi <takayukister@gmail.com>
2026-01-04 17:14:37 +0900

    Update index.js

--- rules/index.js
+++ rules/index.js
@@ -1,24 +1,51 @@
-export { AllRule } from './all';
-export { AnyRule } from './any';
-export { DateRule } from './date';
-export { DayofweekRule } from './dayofweek';
-export { EmailRule } from './email';
-export { EnumRule } from './enum';
-export { FileRule } from './file';
-export { MaxDateRule } from './maxdate';
-export { MaxFilesizeRule } from './maxfilesize';
-export { MaxItemsRule } from './maxitems';
-export { MaxLengthRule } from './maxlength';
-export { MaxNumberRule } from './maxnumber';
-export { MinDateRule } from './mindate';
-export { MinFilesizeRule } from './minfilesize';
-export { MinItemsRule } from './minitems';
-export { MinLengthRule } from './minlength';
-export { MinNumberRule } from './minnumber';
-export { NumberRule } from './number';
-export { RequiredRule } from './required';
-export { RequiredFileRule } from './requiredfile';
-export { StepNumberRule } from './stepnumber';
-export { TelRule } from './tel';
-export { TimeRule } from './time';
-export { URLRule } from './url';
+import { AllRule } from './all';
+import { AnyRule } from './any';
+import { DateRule } from './date';
+import { DayofweekRule } from './dayofweek';
+import { EmailRule } from './email';
+import { EnumRule } from './enum';
+import { FileRule } from './file';
+import { MaxDateRule } from './maxdate';
+import { MaxFilesizeRule } from './maxfilesize';
+import { MaxItemsRule } from './maxitems';
+import { MaxLengthRule } from './maxlength';
+import { MaxNumberRule } from './maxnumber';
+import { MinDateRule } from './mindate';
+import { MinFilesizeRule } from './minfilesize';
+import { MinItemsRule } from './minitems';
+import { MinLengthRule } from './minlength';
+import { MinNumberRule } from './minnumber';
+import { NumberRule } from './number';
+import { RequiredRule } from './required';
+import { RequiredFileRule } from './requiredfile';
+import { StepNumberRule } from './stepnumber';
+import { TelRule } from './tel';
+import { TimeRule } from './time';
+import { URLRule } from './url';
+
+export const rules = new Map( [
+	[ 'all', AllRule ],
+	[ 'any', AnyRule ],
+	[ 'date', DateRule ],
+	[ 'dayofweek', DayofweekRule ],
+	[ 'email', EmailRule ],
+	[ 'enum', EnumRule ],
+	[ 'file', FileRule ],
+	[ 'maxdate', MaxDateRule ],
+	[ 'maxfilesize', MaxFilesizeRule ],
+	[ 'maxitems', MaxItemsRule ],
+	[ 'maxlength', MaxLengthRule ],
+	[ 'maxnumber', MaxNumberRule ],
+	[ 'mindate', MinDateRule ],
+	[ 'minfilesize', MinFilesizeRule ],
+	[ 'minitems', MinItemsRule ],
+	[ 'minlength', MinLengthRule ],
+	[ 'minnumber', MinNumberRule ],
+	[ 'number', NumberRule ],
+	[ 'required', RequiredRule ],
+	[ 'requiredfile', RequiredFileRule ],
+	[ 'stepnumber', StepNumberRule ],
+	[ 'tel', TelRule ],
+	[ 'time', TimeRule ],
+	[ 'url', URLRule ],
+] );
