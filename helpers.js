6045267debbce676faa39084486d34180ee176a0
Takayuki Miyoshi <takayukister@gmail.com>
2026-01-17 12:16:50 +0900

    Simplify flattenTree()

--- helpers.js
+++ helpers.js
@@ -3,35 +3,23 @@
  * Converts a multi-layered object to a flat array.
  */
 export const flattenTree = ( tree ) => {
-	if ( Object === tree.constructor ) {
-		const result = [];
+	const result = [];
 
+	if ( Object === tree.constructor ) {
 		for ( const value of Object.values( tree ) ) {
 			result.push( ...flattenTree( value ) );
 		}
-
-		return result;
-	}
-
-	if ( tree instanceof Map ) {
-		const result = [];
-
+	} else if ( tree instanceof Map ) {
 		for ( const [ key, value ] of tree ) {
 			result.push( ...flattenTree( value ) );
 		}
-
-		return result;
-	}
-
-	if ( Array.isArray( tree ) ) {
-		const result = [];
-
+	} else if ( Array.isArray( tree ) ) {
 		for ( const value of tree ) {
 			result.push( ...flattenTree( value ) );
 		}
-
-		return result;
+	} else {
+		result.push( tree );
 	}
 
-	return [ tree ];
+	return result;
 };
