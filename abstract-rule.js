3644d8fc752a2089e64bf1ce23d0b25e09e8bc88
Takayuki Miyoshi <takayukister@gmail.com>
2026-01-18 11:35:07 +0900

    Implement toJSON()

--- abstract-rule.js
+++ abstract-rule.js
@@ -1,9 +1,28 @@
 export function AbstractRule() {}
 
-AbstractRule.prototype.matches = function ( context ) {
-	return true;
-}
 
-AbstractRule.prototype.validate = function ( formDataTree, context ) {
-	return true;
-}
+AbstractRule.prototype = {
+
+	matches( context ) {
+		return true;
+	},
+
+	validate( formDataTree, context ) {
+		return true;
+	},
+
+	toJSON() {
+		const properties = [];
+
+		Object.entries( this ).forEach( ( [ key, value ] ) => {
+			properties.push( [ key, value ] );
+		} );
+
+		if ( this.constructor.RULE_NAME ) {
+			properties.unshift( [ 'rule', this.constructor.RULE_NAME ] );
+		}
+
+		return Object.fromEntries( properties );
+	},
+
+};
--- composite-rule.js
+++ composite-rule.js
@@ -1,17 +1,41 @@
 import { AbstractRule } from './abstract-rule';
 
-export function CompositeRule() {
-	this.rules = [];
-}
+export function CompositeRule() {}
 
-CompositeRule.prototype.addRule = function ( rule ) {
-	if ( ! ( rule instanceof AbstractRule ) ) {
-		throw new TypeError( "'rule' is not a rule object" );
-	}
 
-	this.rules.push( rule );
+CompositeRule.prototype = {
 
-	return true;
-}
+	rules: [],
+
+	addRule( rule ) {
+		if ( ! ( rule instanceof AbstractRule ) ) {
+			throw new TypeError( "'rule' is not a rule object" );
+		}
+
+		this.rules.push( rule );
+
+		return true;
+	},
+
+	toJSON() {
+		const properties = [];
+
+		Object.entries( this ).forEach( ( [ key, value ] ) => {
+			if ( 'rules' === key ) {
+				properties.push( [ key, value.map( rule => rule.toJSON() ) ] );
+			} else {
+				properties.push( [ key, value ] );
+			}
+		} );
+
+		if ( this.constructor.RULE_NAME ) {
+			properties.unshift( [ 'rule', this.constructor.RULE_NAME ] );
+		}
+
+		return Object.fromEntries( properties );
+	},
+
+};
+
 
 Object.setPrototypeOf( CompositeRule.prototype, AbstractRule.prototype );
