92b8c45c0c9fec579f97dad34d54c2ae3dd64bee
Takayuki Miyoshi <takayukister@gmail.com>
2026-08-08 16:24:26 +0900

    Create enum.js
    #43

---
+++ test/rules/enum.js
@@ -0,0 +1,80 @@
+import { strict as assert } from "node:assert";
+import FormDataTree from "@rocklobsterinc/form-data-tree";
+import { rulesDictionary } from "../../rules/index.js";
+import { InvalidityException } from "../../invalidity-exception.js";
+
+const ruleName = "enum";
+const ruleConstructor = rulesDictionary.get(ruleName);
+
+const valids = [
+  { value: "", accept: ["a", "b", "c"] }, // blank
+  { value: "c", accept: ["a", "b", "c"] }, // valid
+];
+
+const invalids = [
+  { value: "z", accept: ["a", "b", "c"] }, // invalid
+];
+
+describe(ruleConstructor.name, function () {
+  const testValidity = ({ value, accept }) =>
+    function () {
+      const rule = new ruleConstructor({
+        field: "the-field-name",
+        accept,
+      });
+
+      const formData = new FormData();
+      formData.append("the-field-name", value);
+
+      const formDataTree = FormDataTree.from(formData);
+
+      let result;
+
+      try {
+        result = rule.validate(formDataTree);
+      } catch (error) {
+        if (!(error instanceof InvalidityException)) {
+          throw error;
+        }
+      }
+
+      assert(result);
+    };
+
+  const testInvalidity = ({ value, accept }) =>
+    function () {
+      const rule = new ruleConstructor({
+        field: "the-field-name",
+        accept,
+        error: "Just another error message.",
+      });
+
+      const formData = new FormData();
+      formData.append("the-field-name", value);
+
+      const formDataTree = FormDataTree.from(formData);
+
+      let invalidity;
+
+      try {
+        rule.validate(formDataTree);
+      } catch (error) {
+        if (error instanceof InvalidityException) {
+          invalidity = error;
+        } else {
+          throw error;
+        }
+      }
+
+      assert(invalidity instanceof InvalidityException);
+      assert(invalidity.message === rule.error);
+    };
+
+  valids.forEach((params) => {
+    it(`should validate ${JSON.stringify(params)}`, testValidity(params));
+  });
+
+  invalids.forEach((params) => {
+    it(`should invalidate ${JSON.stringify(params)}`, testInvalidity(params));
+  });
+});
